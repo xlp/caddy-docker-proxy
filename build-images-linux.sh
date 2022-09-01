970aef21a56620815e5c9e559a028820b53fc8ca
Alex Passfall <alex.passfall@eternalnet.ch>
2022-09-01 10:27:58 +0200

    update

--- build-images-linux.sh
+++ build-images-linux.sh
@@ -7,7 +7,8 @@ docker run --privileged --rm tonistiigi/binfmt --install all
 
 find artifacts/binaries -type f -exec chmod +x {} \;
 
-PLATFORMS="linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
+#PLATFORMS="linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
+PLATFORMS="linux/amd64"
 OUTPUT="type=local,dest=local"
 TAGS=
 TAGS_ALPINE=
@@ -15,11 +16,10 @@ TAGS_ALPINE=
 if [[ "${BUILD_SOURCEBRANCH}" == "refs/heads/master" ]]; then
     echo "Building and pushing CI images"
 
-    docker login -u lucaslorentz -p "$DOCKER_PASSWORD"
 
     OUTPUT="type=registry"
-    TAGS="-t lucaslorentz/caddy-docker-proxy:ci"
-    TAGS_ALPINE="-t lucaslorentz/caddy-docker-proxy:ci-alpine"
+    TAGS="-t docker.eternalnet.ch/caddy-docker-proxy:ci"
+    TAGS_ALPINE="-t docker.eternalnet.ch/caddy-docker-proxy:ci-alpine"
 fi
 
 if [[ "${BUILD_SOURCEBRANCH}" =~ ^refs/tags/v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]]; then
@@ -27,18 +27,18 @@ if [[ "${BUILD_SOURCEBRANCH}" =~ ^refs/tags/v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]]; t
 
     echo "Releasing version ${RELEASE_VERSION}..."
 
-    docker login -u lucaslorentz -p "$DOCKER_PASSWORD"
+    docker login -u docker.eternalnet.ch -p "$DOCKER_PASSWORD"
 
     PATCH_VERSION=$(echo $RELEASE_VERSION | cut -c2-)
     MINOR_VERSION=$(echo $PATCH_VERSION | cut -d. -f-2)
 
     OUTPUT="type=registry"
-    TAGS="-t lucaslorentz/caddy-docker-proxy:latest \
-        -t lucaslorentz/caddy-docker-proxy:${PATCH_VERSION} \
-        -t lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}"
-    TAGS_ALPINE="-t lucaslorentz/caddy-docker-proxy:alpine \
-        -t lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-alpine \
-        -t lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-alpine"
+    TAGS="-t docker.eternalnet.ch/caddy-docker-proxy:latest \
+        -t docker.eternalnet.ch/caddy-docker-proxy:${PATCH_VERSION} \
+        -t docker.eternalnet.ch/caddy-docker-proxy:${MINOR_VERSION}"
+    TAGS_ALPINE="-t docker.eternalnet.ch/caddy-docker-proxy:alpine \
+        -t docker.eternalnet.ch/caddy-docker-proxy:${PATCH_VERSION}-alpine \
+        -t docker.eternalnet.ch/caddy-docker-proxy:${MINOR_VERSION}-alpine"
 fi
 
 docker buildx build -f Dockerfile . \
